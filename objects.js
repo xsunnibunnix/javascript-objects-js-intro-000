608aa9f1004c278d249e43b6d87e004377a57d19
Sunny Pacheco <pacheco.sunny@gmail.com>
2021-02-10 23:55:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,8 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return playlist[artistName] = songTitle
+  playlist[artistName] = songTitle
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
