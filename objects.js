7e95005824b8a626183d4e39cf53b7392c11c369
Sunny Pacheco <pacheco.sunny@gmail.com>
2021-02-10 23:08:39 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,7 @@ var playlist = {
   theBeatles: "Yesterday",
   billieEilish: "Bad Guy",
 }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.Assign(playlist, {[artistName: songTitle]})
+}
